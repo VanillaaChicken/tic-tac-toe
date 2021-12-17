421d3895389101ca19e3af5dee1201671922a27e
VanillaaChicken <92608536+VanillaaChicken@users.noreply.github.com>
2021-12-17 07:52:18 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,103 @@
+# AEUAEUUEAUEUUEUE
+import random
+import sys
+# The lil a, uhhhh, the ummmmmm, board paper, uuuhhhhh
+board = [i for i in range(0, 9)]
+# compooter
+player, computer = "", ""
+# Moves, Winz (ez)
+movez = ((1, 7, 2, 9),(5,),(2, 4, 6, 8))
+winz = ((0, 1, 2),(3, 4, 5),(6, 7, 8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+# our table but its not broken (i hope)
+tab = range(1 , 10)
+# Displey ze boerd
+def print_board():
+    x = 1
+    for i in board:
+        end = " | "
+        if x%3 == 0:
+            end = ' \n'
+            if i != 1: end += '---------\n';
+        char = ' '
+        if i in ('X', 'O'): char = i;
+        x += 1
+        print(char, end = end)
+# chars of sum idek
+def select_char():
+    chars = ('X', 'O')
+    if random.randint(0,1) == 0:
+        return chars[::-1]
+    return chars
+# were u canz moveeeee
+def can_move(brd, player, move):
+    if move in tab and brd[move-1] == move-1:
+        return True
+    return False
+# were u can winnnnn (ez + gg + no dubs + cranking 90's + L + ratio + bozo + *clown emoji*)
+def can_win(brd, player, move):
+    places=[]
+    x=0
+    for i in brd:
+        if i == player: places.append(x);
+        x+=1
+    win=True
+    for tup in winz:
+        win=True
+        for ix in tup:
+            if brd[ix] != player:
+                win=False
+                break
+        if win == True:
+            break
+    return win
+def make_move(brd, player, move, undo=False):
+    if can_move(brd, player, move):
+        brd[move-1] = player
+        win=can_win(brd, player, move)
+        if undo:
+            brd[move-1] = move-1
+        return (True, win)
+    return (False, False)
+# AIE EYE
+def computer_move():
+    move=-1
+    # so if i can winz, it no matter
+    for i in range(1,10):
+        if make_move(board, computer, i, True)[1]:
+            move=i
+            break
+    if move == -1:
+        # uno bloc card
+        for i in range(1,10):
+            if make_move(board, player, i, True)[1]:
+                move=i
+                break
+    if move == -1:
+        # move thin
+        for tup in movez:
+            for mv in tup:
+                if move == -1 and can_move(board, computer, mv):
+                    move=mv
+                    break
+    return make_move(board, computer, move)
+def space_exist():
+    return board.count('X') + board.count('O') != 9
+player, computer = select_char()
+print('Player is [%s] and computer is [%s]' % (player, computer))
+result='oof lol lose lose!'
+while space_exist():
+    print_board()
+    print('> Make your move ! [1-9] : ', end='')
+    move = int(input())
+    moved, won = make_move(board, player, move)
+    if not moved:
+        print('! Invalid number ! Try again !')
+        continue
+    if won:
+        result='*** HAHAHAHAHHA LOL COMPOOTER JUST GOT CLAPPED LOL L + RATIO + BOZO + GET GOOD + EZ + GG + NO DUBS ***'
+        break
+    elif computer_move()[1]:
+        result='=== hax =='
+        break;
+print_board()
+print(result)
